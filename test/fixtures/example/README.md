43e4b11558c61d765bff5360a7bcc0693069f041
Krzysztof Kaczor <krzkaczor@gmail.com>
2020-12-15 20:16:28 +0100

    Add more integration tests

--- test/fixtures/example/README.md
+++ test/fixtures/example/README.md
@@ -1,6 +1,6 @@
 # Boilerplate
 
-[![All Contributors](https://img.shields.io/badge/all_contributors-0-orange.svg?style=flat-square)](#contributors)
+[![All Contributors](https://img.shields.io/badge/all_contributors-1-orange.svg?style=flat-square)](#contributors)
 
 ## Contributors ✨
 
@@ -8,4 +8,6 @@ Thanks goes to these wonderful people ([emoji key](https://allcontributors.org/d
 
 <!-- ALL-CONTRIBUTORS-LIST:START - Do not remove or modify this section -->
 <!-- prettier-ignore -->
+<table><tr><td align="center"><a href="https://github.com/krzkaczor"><img src="https://avatars2.githubusercontent.com/u/1814312?v=4" width="100px;" alt="Kris Kaczor"/><br /><sub><b>Kris Kaczor</b></sub></a><br /><a href="https://github.com/krzkaczor/gha-playground/commits?author=krzkaczor" title="Code">💻</a> <a href="https://github.com/krzkaczor/gha-playground/commits?author=krzkaczor" title="Documentation">📖</a></td></tr></table>
+
 <!-- ALL-CONTRIBUTORS-LIST:END -->
--- test/integration.test.ts
+++ test/integration.test.ts
@@ -12,7 +12,7 @@ import { nockTest } from './helpers/nockTest'
 
 describe('integration', () => {
   it(
-    'works',
+    'adds a new contributor',
     nockTest(async () => {
       // prepare directory with dummy allcontributors setup
       const workdirPath = tmp.tmpNameSync()
@@ -30,8 +30,31 @@ describe('integration', () => {
       const allContributorsCfg = JSON.parse(readFileSync(join(workdirPath, '.all-contributorsrc'), 'utf-8'))
       const exampleReadme = readFileSync(join(workdirPath, 'README.md'), 'utf8')
 
-      expect(allContributorsCfg.contributors.length).toEqual(1)
+      expect(allContributorsCfg.contributors.length).toEqual(2)
       expect(exampleReadme).toEqual(expect.stringMatching('monalisa octocat'))
+    }),
+  )
+
+  it(
+    'modifies existing contributions',
+    nockTest(async () => {
+      // prepare directory with dummy allcontributors setup
+      const workdirPath = tmp.tmpNameSync()
+      copySync(join(__dirname, './fixtures/example'), workdirPath)
+
+      const octokit = getOctokit('NOT_EXISTING_TOKEN')
+      const execMock = mockFn<Exec>().resolvesTo(0)
+      // event from an admin to add @octocat user as a contributor
+      const event = require('./fixtures/validEvent.json')
+      event.payload.comment.body = '@all-contributors please add @krzkaczor for audio'
+      const ctx = new Context(event)
+
+      await action({ cwd: workdirPath, exec: execMock, octokit, ctx })
+
+      // assert that a new contributor was added and readme regenerated
+      const allContributorsCfg = JSON.parse(readFileSync(join(workdirPath, '.all-contributorsrc'), 'utf-8'))
+
+      expect(allContributorsCfg.contributors[0]).toEqual(expect.objectWith({ contributions: ['code', 'doc', 'audio'] }))
     }),
   )
 })
